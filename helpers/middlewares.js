c569887fee1b3b6bf442bb1345a5d2d397106ee6
Kai Sing <kaising.n@gmail.com>
2020-05-06 14:18:01 -0700

    no db model

---
+++ helpers/middlewares.js
@@ -0,0 +1,22 @@
+function mustBeInteger(req, res, next) {
+  const id = req.params.id;
+  if (!Number.isInteger(parseInt(id))) {
+    res.status(400).json({ message: "ID must be an integer" });
+  } else {
+    next();
+  }
+}
+
+function checkFieldsPost(req, res, next) {
+  const { author, data, origin } = req.body;
+  if (author && data && origin) {
+    next();
+  } else {
+    res.status(400).json({ message: "fields are not good" });
+  }
+}
+
+module.exports = {
+  mustBeInteger,
+  checkFieldsPost,
+};
